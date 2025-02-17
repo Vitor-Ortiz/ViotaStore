3c5f1e549cc4476ff746adcdabf1af28d19e6358
Vitor-Ortiz <Vitorortizoficial@gmail.com>
2025-02-17 08:01:42 -0300

    Criado a camada de dados

---
+++ ViotaStore/Models/Categoria.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ViotaStore.Models;
+
+[Table("categoria")]
+public class Categoria
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int ID { get; set; }
+
+    [Required(ErrorMessage = "Por favor deixe de ser filha da puta e digite essa porra , obrigado")]
+    [StringLength(30, ErrorMessage = "O nome deve possuir no maximo 30 caracteres , filha da puta")]
+    public string Nome { get; set; }
+
+    [StringLength(300)]
+    public string Foto { get; set; }
+}
---
+++ ViotaStore/Models/Produto.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ViotaStore.Models;
+
+[Table("Produto")]
+public class Produto
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Por favor , informe a Categoria")]
+    public int CategoriaId { get; set; }
+    [ForeignKey("CategoriaId")]
+
+    public Categoria Categoria { get; set; }
+
+    [Required(ErrorMessage = "Por favor deixe de ser filha da puta e digite essa porra , obrigado")]
+    [StringLength(60, ErrorMessage = "O nome deve possuir no maximo 60 caracteres , filha da puta")]
+    public string nome { get; set; }
+
+    [Display(Name = "Desciçao", Prompt = "Desciçao")]
+    [StringLength(1000, ErrorMessage = "A Desciçao deve possuir no maximo 100caracteres , cuzao")]
+
+    public string Descricao { get; set; }
+    [Display(Name = "Quantidade em estoque")]
+    [Range(0, int.MaxValue)]
+    [StringLength(1000, ErrorMessage = "Por favor , informe a quantidade em estoque")]
+    public int QtEstoque { get; set; }
+    [Display(Name = "Valor de Custo")]
+    [Range(0, double.MaxValue)]
+    [Column(TypeName = "numeric(10,2)")]
+    public decimal ValorCusto { get; set; }
+       [Display(Name = "Valor de Custo")]
+    [Range(0, double.MaxValue)]
+    [Column(TypeName = "numeric(10,2)")]
+    public decimal ValorVenda { get; set; }
+}
