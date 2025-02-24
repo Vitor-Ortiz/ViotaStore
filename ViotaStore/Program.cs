5dc51fc5246536199648e80b53bcb2c5721baf67
Vitor-Ortiz <Vitorortizoficial@gmail.com>
2025-02-24 08:02:50 -0300

    finalizada a conexao com o banco de dados

--- ViotaStore/Program.cs
+++ ViotaStore/Program.cs
@@ -1,9 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ViotaStore.Data;
+using ViotaStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//configuração do serviço de conexao com o banco de dados
+string conexao = builder.Configuration.GetConnectionString("ViotaStoreConn");
+builder.Services.AddDbContext<AppDbContext>(
+
+    options => options.UseMySQL(conexao)
+);
+
+//configuração do serviço de identidade de usuarios
+builder.Services.AddIdentity<Usuario, IdentityRole>(
+    options => options.SignIn.RequireConfirmedEmail = false
+).AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+using(var scope = app.Services.CreateAsyncScope()) {
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
